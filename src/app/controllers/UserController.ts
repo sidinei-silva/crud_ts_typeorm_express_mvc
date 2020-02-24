8d233248da07c5a5805408769f6aa1fc3b72d89e
Sidinei Silva <sidinei.silva02@gmail.com>
2020-02-24 10:44:57 -0300

    Insert another requests

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -8,6 +8,40 @@ class UserController {
     const users = await User.find();
     return res.json(users);
   }
+
+  async show(req: Request, res: Response): Promise<Response> {
+    const User = getCustomRepository(UserRepository);
+    const user = await User.findOne(req.params.userId);
+    return res.json(user);
+  }
+
+  async store(req: Request, res: Response): Promise<Response> {
+    const User = getCustomRepository(UserRepository);
+    const user = await User.create(req.body);
+    const results = await User.save(user);
+    return res.json(results);
+  }
+
+  async update(req: Request, res: Response): Promise<Response> {
+    const User = getCustomRepository(UserRepository);
+    const user = await User.findOne(req.params.userId);
+
+    if (!user) {
+      return res.status(400).json({ error: 'User not found' });
+    }
+
+    User.merge(user, req.body);
+    const results = await User.save(user);
+
+    return res.json(results);
+  }
+
+  async delete(req: Request, res: Response): Promise<Response> {
+    const User = getCustomRepository(UserRepository);
+    const results = await User.delete(req.params.userId);
+
+    return res.json(results);
+  }
 }
 
 export default new UserController();
--- src/routes.ts
+++ src/routes.ts
@@ -9,5 +9,9 @@ routes.get('/', (req, res) => {
 });
 
 routes.get('/users', UserController.index);
+routes.get('/users/:userId', UserController.show);
+routes.post('/users', UserController.store);
+routes.put('/users/:userId', UserController.update);
+routes.delete('/users/:userId', UserController.delete);
 
 export default routes;
