22edbc20d029a7d5337c3d6ded9d9430b9f6d573
Sidinei Silva <sidinei.silva02@gmail.com>
2020-02-24 00:58:12 -0300

    Include controller

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+
 const routes = Router();
 
 routes.get('/', (req, res) => {
   return res.json({ message: 'Hello Routes' });
 });
+
+routes.get('/users', UserController.index);
 
 export default routes;
