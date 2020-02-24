36442c788c291ea73fe7000e388bd8211c172195
Sidinei Silva <sidinei.silva02@gmail.com>
2020-02-24 01:46:07 -0300

    Include promise in file controller user

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -3,9 +3,9 @@ import { getCustomRepository } from 'typeorm';
 import { UserRepository } from '../models/UserRepository';
 
 class UserController {
-  index(req: Request, res: Response): Response {
+  async index(req: Request, res: Response): Promise<Response> {
     const User = getCustomRepository(UserRepository);
-    const users = User.find();
+    const users = await User.find();
     return res.json(users);
   }
 }
